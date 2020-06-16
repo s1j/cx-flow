afeda9b7101a260fd6358c0aaf941832d320e2f0
Ofer Skolsky <Ofer.Skolsky@checkmarx.com>
2020-06-16 15:32:35 +0300

    fixed access control of e2e sca tests

--- src/test/java/com/checkmarx/flow/cucumber/integration/end2end/genericendtoend/GenericEndToEndSteps.java
+++ src/test/java/com/checkmarx/flow/cucumber/integration/end2end/genericendtoend/GenericEndToEndSteps.java
@@ -88,7 +88,7 @@ public class GenericEndToEndSteps {
             ScaProperties scaProperties = (ScaProperties)appContext.getBean("scaProperties");
             scaProperties.setAppUrl("https://sca.scacheckmarx.com");
             scaProperties.setApiUrl("https://api.scacheckmarx.com");
-            scaProperties.setAccessControlUrl("https://v2.ac-checkmarx.com");
+            scaProperties.setAccessControlUrl("https://platform.checkmarx.net");
         }
     }
 
